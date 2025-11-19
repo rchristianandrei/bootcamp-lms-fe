cf1b1cfbd82de66ef696b312ec25aba418d96fa7
Christian Andrei Reyes <careyes@sscgi.com>
2025-11-19 11:59:07 +0800

    dashboard ui design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,63 @@
-import Image from "next/image";
+import { CourseCard } from "@/components/custom/custom-card";
 
 export default function Home() {
+  const courses = [
+    {
+      name: "Data Structures and Algorithms",
+      description: "Lorem ipsum dolor sit amet",
+      progress: 100
+    },
+    {
+      name: "Agile Methodology and Practices",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi aperiam reprehenderit id. Obcaecati ducimus sunt repellendus tenetur autem in itaque. Pariatur ullam maxime possimus ex neque sit ea blanditiis iure?",
+        progress: 40
+    },
+    {
+      name: "ASP.NET Web Api",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi aperiam reprehenderit id. Obcaecati ducimus sunt repellendus tenetur autem in itaque. Pariatur ullam maxime possimus ex neque sit ea blanditiis iure?",
+        progress: 20
+    },
+    {
+      name: "Fundamentals of Angular",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi aperiam reprehenderit id. Obcaecati ducimus sunt repellendus tenetur autem in itaque. Pariatur ullam maxime possimus ex neque sit ea blanditiis iure?",
+        progress: 15
+    },
+  ];
+
   return (
-    <>BOOTCAMP LEARNING MANAGEMENT SYSTEM</>
+    <>
+      <header className="bg-green-800 text-white px-3 py-5 flex justify-between items-center">
+        <h1 className="text-4xl">SSCGI LMS</h1>
+        <div className="text-right">
+          <p>Reyes, Christian</p>
+          <p className="text-sm">Student</p>
+        </div>
+      </header>
+      <main className="py-5 px-3 flex flex-col gap-5">
+        <section className="max-w-[1250px] mx-auto flex flex-col gap-2">
+          <h2 className="text-3xl">Your Courses</h2>
+          <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2">
+            {courses.map((course, key) => (
+              <CourseCard key={key} courseDetails={course}></CourseCard>
+            ))}
+          </section>
+        </section>
+
+        <section className="max-w-[1250px] mx-auto flex flex-col gap-2">
+          <h2 className="text-3xl">Completed Courses</h2>
+          <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2">
+            {courses.map((course, key) => (
+              <CourseCard key={key} courseDetails={course}></CourseCard>
+            ))}
+          </section>
+        </section>
+      </main>
+      <footer className="bg-gray-700 text-white px-3 py-5">
+        All Rights Reserved
+      </footer>
+    </>
   );
 }
---
+++ src/components/custom/custom-card.tsx
@@ -0,0 +1,32 @@
+function randomLightColor() {
+  const h = Math.floor(Math.random() * 360);   // any hue
+  const s = 70;                                // good saturation
+  const l = 85;                                // high lightness = light colors
+  return `hsl(${h}, ${s}%, ${l}%)`;
+}
+
+export function CourseCard(props: {
+  courseDetails: { name: string; description: string; progress: number };
+}) {
+  return (
+    <>
+      <section className="border rounded flex flex-col" style={{backgroundColor: `${randomLightColor()}`}}>
+        <section className="border-b-1 px-1 hover:underline cursor-pointer">
+          <h3 className="text-lg font-bold">{props.courseDetails.name}</h3>
+        </section>
+        <p className="p-1 line-clamp-5 flex-1 text-sm">
+          {props.courseDetails.description}
+        </p>
+        <section className="border-t-1 px-1 flex items-center gap-1">
+          <div className="rounded h-3 flex-1">
+            <div
+              className="bg-green-600 rounded h-full"
+              style={{ width: `${props.courseDetails.progress}%` }}
+            ></div>
+          </div>
+        <p>{props.courseDetails.progress}%</p>
+        </section>
+      </section>
+    </>
+  );
+}
